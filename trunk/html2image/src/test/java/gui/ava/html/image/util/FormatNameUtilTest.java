d86caf07774300ffe3ec6c4513c13c6b644e70d7
yoava@alphacsp.com <yoava@alphacsp.com@c99cf827-068f-e4d5-d774-4a40a78301c4>
2010-07-06 06:50:37 +0000

    added html2image sources

---
+++ trunk/html2image/src/test/java/gui/ava/html/image/util/FormatNameUtilTest.java
@@ -0,0 +1,46 @@
+package gui.ava.html.image.util;
+
+import gui.ava.html.image.util.FormatNameUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Yoav Aharoni
+ */
+public class FormatNameUtilTest {
+	@Test
+	public void testGif() {
+		final String format = FormatNameUtil.formatForFilename("test.file.gif");
+		Assert.assertEquals("gif", format);
+	}
+
+	@Test
+	public void testPng() {
+		final String format = FormatNameUtil.formatForFilename("test.file.png");
+		Assert.assertEquals("png", format);
+	}
+
+	@Test
+	public void testJpg() {
+		final String format = FormatNameUtil.formatForFilename("test.file.jpg");
+		Assert.assertEquals("jpg", format);
+	}
+
+	@Test
+	public void testNoName() {
+		final String format = FormatNameUtil.formatForFilename(".gif");
+		Assert.assertEquals("gif", format);
+	}
+
+	@Test
+	public void testNoExt() {
+		final String format = FormatNameUtil.formatForFilename("name.");
+		Assert.assertEquals("png", format);
+	}
+
+	@Test
+	public void testEmptyFilename() {
+		final String format = FormatNameUtil.formatForFilename("");
+		Assert.assertEquals("png", format);
+	}
+}
